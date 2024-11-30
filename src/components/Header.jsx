db255a4de255c9ed855d21e5dbfec8937125c8f8
Julia Zanotta <julia_zanotta@hotmail.com>
2024-11-30 15:22:17 -0300

    menu hamburger mobile etc

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,29 +7,57 @@ import { NovoLocal } from "./NovoLocal";
 
 export function Header({ local, locais, setLocais }) {
     const [open, setOpen] = useState(false)
+    const [ menuOpen, setMenuOpen ] = useState(false)
 
     function open_cadastrar() {
         setOpen(true)
     }
 
+    function clickMenu() {
+        setMenuOpen(!menuOpen); 
+    }
+
     return (
         <header>
             <div className="nav">
+                <img src="./burger.png" className="burger" />
                 <p><Link to='/formulario' className="nav_link">Encontre</Link></p>
                 <p><Link to='/favoritos' className="nav_link"
-                local={local}
-                locais={locais}
-                setLocais={setLocais}
+                    local={local}
+                    locais={locais}
+                    setLocais={setLocais}
                 >Favoritos</Link></p>
             </div>
             <h2><Link to='/' className="logo">Sul<span>Destino</span></Link></h2>
             <div className="header_botoes">
                 <div className="lupa_wrapper">
-                    <Link to='/pesquisar'><img src="./lupa.png" className="header_lupa"/></Link>
+                    <Link to='/pesquisar'><img src="./lupa.png" className="header_lupa" /></Link>
                 </div>
                 <button onClick={open_cadastrar} className="nav_botao">Cadastre um novo destino</button>
+            </div>
+
+
+
+
+
+            <div className="header_mobile">
+                <h2><Link to='/' className="logo_mobile">Sul<span>Destino</span></Link></h2>
+                <div className="nav_mobile">
+                    <img src="./burger.png" className="menu_icone_mobile" onClick={clickMenu} />
+                    <Link to='/pesquisar' className="lupa_mobile"><img src="./lupa.png" /></Link>
+                </div>
+                <div className={menuOpen ? "menu_open" : "menu_closed"}>
+                    <p><Link to='/formulario' className="nav_link_mobile">Encontre</Link></p>
+                    <p><Link to='/favoritos' className="nav_link_mobile"
+                        local={local}
+                        locais={locais}
+                        setLocais={setLocais}
+                    >Favoritos</Link></p>
+                    <button onClick={open_cadastrar} className='nav_botao_mobile'>Cadastre um novo destino</button>
+                </div>
 
             </div>
+
 
             <Modal open={open} onClose={() => setOpen(false)} center>
                 <NovoLocal
